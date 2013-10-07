47819958be6d932547d57c66792c9ec058bd83b6
JeongInnLee <aretias.kr@gmail.com>
2013-10-07 18:46:52 +0900

    Update HelloWorldUnitTest.java

--- src/test/java/HelloWorldUnitTest.java
+++ src/test/java/HelloWorldUnitTest.java
@@ -2,13 +2,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/*
- * @(#)HelloWorldUnitTest.java $version 2013. 10. 7.
- *
- * Copyright 2007 NHN Corp. All rights Reserved. 
- * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
 /**
  * @author JeongInn
  */
