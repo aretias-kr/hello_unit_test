0bdbde875d24b6c250039e1d61cd615a71fbc043
JeongInn Lee <aretias.kr@gmail.com>
2013-10-07 18:33:09 +0900

    Initial Commit for Hello unit test

---
+++ src/test/java/HelloWorldUnitTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*
+ * @(#)HelloWorldUnitTest.java $version 2013. 10. 7.
+ *
+ * Copyright 2007 NHN Corp. All rights Reserved. 
+ * NHN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author JeongInn
+ */
+public class HelloWorldUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
