d9ebcb4e0dce13fa8d464bc599865d7f00664662
JeongInn Lee <aretias.kr@gmail.com>
2013-10-07 18:40:20 +0900

    create test case for hello_unit_tests

--- src/test/java/HelloWorldUnitTest.java
+++ src/test/java/HelloWorldUnitTest.java
@@ -13,10 +13,9 @@ import org.junit.Test;
  * @author JeongInn
  */
 public class HelloWorldUnitTest {
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void hello_unit_tests() {
+		String helloWorld = "Hello" + " World!";
+		assertEquals("Hello World!", helloWorld);
 	}
-
 }
